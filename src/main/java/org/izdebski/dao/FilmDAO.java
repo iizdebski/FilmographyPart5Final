1ded492d8fa79a6b99f888a9cdd27c6693cedba0
ivan <iizdepskyy112@gmail.com>
2019-09-22 12:14:51 +0300

    Filmography final

---
+++ src/main/java/org/izdebski/dao/FilmDAO.java
@@ -0,0 +1,17 @@
+package org.izdebski.dao;
+
+import org.izdebski.model.Film;
+
+import java.util.List;
+
+public interface FilmDAO {
+    List<Film> allFilms(int page);
+    void add(Film film);
+    void delete(Film film);
+    void edit(Film film);
+    Film getById(int id);
+
+    int filmsCount();
+
+    boolean checkTitle(String title);
+}
